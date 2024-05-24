031407ba9a341184d01bb64237a72da1ea713063
Sardorbek Oqmardiyev <oqmardiyev@gmail.com>
2024-05-24 11:56:14 +0300

    feat: add body

---
+++ index.js
@@ -0,0 +1,16 @@
+let number1 = 2;
+let operator = "*";
+let number2 = 3;
+
+if (operator == "+") {
+  console.log(number1 + number2);
+} else if (operator == "-") {
+  console.log(number1 - number2);
+} else if (operator == "*") {
+  console.log(number1 * number2);
+} else if (operator == "/") {
+  console.log(number1 / number2);
+}else{
+    console.log("bunday ishora yo`q!")
+}
+
